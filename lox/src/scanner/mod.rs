8b6611dbdff42790aaa5ea482037d4b6da345a7b
Nate Catelli <ncatelli@packetfire.org>
2020-04-02 17:23:48 -0500

    WIP: get it compiling

--- lox/src/scanner/mod.rs
+++ lox/src/scanner/mod.rs
@@ -1,46 +1,53 @@
-use std::iter::Peekable;
-
 pub mod tokens;
 
 use tokens::{Token, TokenType};
+
+struct PreToken {
+    literal: char,
+    column: u64,
+    line: u64,
+}
+
+impl PreToken {
+    pub fn new(literal: char, column: u64, line: u64) -> PreToken {
+        PreToken {
+            literal: literal,
+            column: column,
+            line: line,
+        }
+    }
+}
 
 pub struct Scanner {
     source: String,
     start: u64,
     end: usize,
-    current: u64,
-    line: u64, 
 }
 
 impl Scanner {
     pub fn new(source: String) -> Scanner {
         let end = source.len();
 
-        Scanner{
+        Scanner {
             source: source,
             start: 0,
             end: end,
-            current: 0,
-            line: 1,
         }
     }
 
     pub fn scan_tokens(&mut self) -> Vec<Token> {
         let mut tokens: Vec<Token> = Vec::new();
-        let mut char_iter = self.source.chars().peekable();
+        let pt = self.preparse_tokens();
+        let mut pti = pt.into_iter(); 
 
-        while let Some(_) = char_iter.peek() {
-            let next_token = self.scan_token(&mut char_iter);
-            tokens.push(next_token);
-        }
+        while let Some(c) = pti.next() {}
 
-        tokens.push(Token::new(TokenType::EOF, "".to_string(), self.line));
+        tokens.push(Token::new(TokenType::EOF, "".to_string(), 0));
         tokens
     }
 
-    fn scan_token(&self, ci: &mut Peekable<std::str::Chars>) -> Token {
-        let c = ci.next().unwrap();
-        let token_type = match c {
+    fn scan_token(&self, c: PreToken) -> Token {
+        let token_type = match c.literal {
             '(' => TokenType::LeftParen,
             ')' => TokenType::RightParen,
             '{' => TokenType::LeftBrace,
@@ -54,6 +61,29 @@ impl Scanner {
             _ => TokenType::String,
         };
 
-        Token::new(token_type, c.to_string(), self.line)
+        Token::new(token_type, c.literal.to_string(), 0)
+    }
+
+    fn preparse_tokens(&self) -> Vec<PreToken> {
+        let mut line = 0;
+        let mut column = 0;
+
+        self.source.chars().map(|c| {
+            let pt = PreToken {
+                literal: c,
+                line: line,
+                column: column,
+            };
+
+            match c {
+                '\n' => {
+                    column = 0;
+                    line += 1;
+                },
+                _ => column += 1,
+            }
+
+            pt
+        }).collect()
     }
 }
