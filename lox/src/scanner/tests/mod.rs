a720c23043432bd18c76026da26aa666cabff7e5
Nate Catelli <ncatelli@packetfire.org>
2020-04-04 17:23:38 -0500

    add unknown lexeme test.

--- lox/src/scanner/tests/mod.rs
+++ lox/src/scanner/tests/mod.rs
@@ -15,3 +15,15 @@ fn lex_single_tokens_should_return_single_result() {
         })
     );
 }
+
+#[test]
+fn lex_unknown_token_returns_error_result() {
+    let mut s = Scanner::new("%".to_string());
+    let token_results = s.scan_tokens();
+
+    assert_eq!(1, token_results.len());
+    assert_eq!(
+        token_results[0],
+        LexResult::Err("Lex error at line: 1, position: 1.".to_string())
+    );
+}
