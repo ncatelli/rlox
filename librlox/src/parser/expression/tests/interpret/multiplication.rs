3cfc3c52fd883f23680ab2bfebced20905be5efc
Nate Catelli <ncatelli@packetfire.org>
2020-05-10 00:09:47 -0500

    Error message for multiplication error.

--- librlox/src/parser/expression/tests/interpret/multiplication.rs
+++ librlox/src/parser/expression/tests/interpret/multiplication.rs
@@ -1,4 +1,4 @@
-use crate::interpreter::Interpreter;
+use crate::interpreter::{Interpreter, InterpreterErr};
 use crate::parser::expression::{Expr, MultiplicationExpr, PrimaryExpr};
 
 #[test]
@@ -14,4 +14,18 @@ fn multiplication_expr_should_evaluate_when_both_operands_are_numbers() {
 
     assert_eq!(Ok(PrimaryExpr::Number(10.0)), product_expr.interpret());
     assert_eq!(Ok(PrimaryExpr::Number(5.0)), division_expr.interpret());
+}
+
+#[test]
+fn multiplication_expr_should_err_when_operands_are_not_numbers() {
+    let expr = MultiplicationExpr::Multiply(
+        Box::new(Expr::Primary(PrimaryExpr::Str("hello".to_string()))),
+        Box::new(Expr::Primary(PrimaryExpr::Str("world".to_string()))),
+    );
+    assert_eq!(
+        Err(InterpreterErr::TypeErr(
+            "Invalid operand for operator: hello * world".to_string()
+        )),
+        expr.interpret()
+    );
 }
