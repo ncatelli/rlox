2c7e542c75eef48d0b174ff5db242ba6f63a350f
Nate Catelli <ncatelli@packetfire.org>
2020-05-24 23:31:38 -0500

    Switch Environment to store runtime objects.

--- librlox/src/environment/tests/mod.rs
+++ librlox/src/environment/tests/mod.rs
@@ -1,4 +1,3 @@
-use crate::ast::expression::Expr;
 use crate::environment::Environment;
 use std::option::Option;
 
@@ -8,14 +7,14 @@ fn environment_should_allow_setting_of_symbols() {
 
     // unset var returns None
     assert_eq!(
-        symtable.define("test".to_string(), Expr::Primary(obj_bool!(true))),
+        symtable.define("test".to_string(), obj_bool!(true)),
         Option::None
     );
 
     // Subsequent returns previous value
     assert_eq!(
-        symtable.define("test".to_string(), Expr::Primary(obj_bool!(true))),
-        Option::Some(Expr::Primary(obj_bool!(true)))
+        symtable.define("test".to_string(), obj_bool!(true)),
+        Option::Some(obj_bool!(true))
     );
 }
 
@@ -24,13 +23,7 @@ fn environment_should_allow_getting_of_symbols() {
     let mut symtable = Environment::new();
     let key = "test".to_string();
 
-    assert_eq!(
-        symtable.define(key.clone(), Expr::Primary(obj_bool!(true))),
-        Option::None
-    );
+    assert_eq!(symtable.define(key.clone(), obj_bool!(true)), Option::None);
 
-    assert_eq!(
-        symtable.get(&key),
-        Option::Some(&Expr::Primary(obj_bool!(true)))
-    );
+    assert_eq!(symtable.get(&key), Option::Some(&obj_bool!(true)));
 }
--- librlox/src/interpreter/tests/expression/variable.rs
+++ librlox/src/interpreter/tests/expression/variable.rs
@@ -8,10 +8,10 @@ fn declaration_statement_should_set_persistent_global_symbol() {
     let mut interpreter = StatefulInterpreter::new();
     interpreter
         .globals
-        .define("a".to_string(), Expr::Primary(obj_number!(1.0)));
+        .define("a".to_string(), obj_number!(1.0));
     interpreter
         .globals
-        .define("b".to_string(), Expr::Primary(obj_number!(2.0)));
+        .define("b".to_string(), obj_number!(2.0));
 
     assert_eq!(
         Ok(()),
