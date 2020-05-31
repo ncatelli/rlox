3390326597552ca513620c021134fecf192fc2b2
Nate Catelli <ncatelli@packetfire.org>
2020-05-30 22:51:52 -0500

    Adding block statement parser test.

--- librlox/src/parser/tests/statement_parser.rs
+++ librlox/src/parser/tests/statement_parser.rs
@@ -8,7 +8,7 @@ use parcel::MatchStatus;
 use std::option::Option;
 
 #[test]
-fn test_parser_can_parse_declaration_stmt() {
+fn parser_can_parse_declaration_stmt() {
     let identifier_token = Token::new(
         TokenType::Identifier,
         1,
@@ -36,6 +36,56 @@ fn test_parser_can_parse_declaration_stmt() {
                 "test".to_string(),
                 Expr::Primary(obj_number!(5.0))
             )]
+        ))),
+        statements().parse(&input)
+    );
+}
+
+#[test]
+fn parser_can_parse_print_stmt() {
+    let literal_token = Token::new(
+        TokenType::Number,
+        1,
+        Option::Some("5.0".to_string()),
+        Option::Some(obj_number!(5.0)),
+    );
+    let input = vec![
+        Token::new(TokenType::Print, 1, Option::None, Option::None),
+        literal_token.clone(),
+        Token::new(TokenType::Semicolon, 1, Option::None, Option::None),
+    ];
+
+    assert_eq!(
+        Ok(MatchStatus::Match((
+            &input[3..],
+            vec![Stmt::Print(Expr::Primary(obj_number!(5.0)))]
+        ))),
+        statements().parse(&input)
+    );
+}
+
+#[test]
+fn parser_can_parse_block_stmt() {
+    let literal_token = Token::new(
+        TokenType::Number,
+        1,
+        Option::Some("5.0".to_string()),
+        Option::Some(obj_number!(5.0)),
+    );
+    let input = vec![
+        Token::new(TokenType::LeftParen, 1, Option::None, Option::None),
+        Token::new(TokenType::Print, 1, Option::None, Option::None),
+        literal_token.clone(),
+        Token::new(TokenType::Semicolon, 1, Option::None, Option::None),
+        Token::new(TokenType::RightParen, 1, Option::None, Option::None),
+    ];
+
+    assert_eq!(
+        Ok(MatchStatus::Match((
+            &input[5..],
+            vec![Stmt::Block(vec![Stmt::Print(Expr::Primary(obj_number!(
+                5.0
+            )))])]
         ))),
         statements().parse(&input)
     );
