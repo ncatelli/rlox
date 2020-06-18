54a5137ab0a997561d023804848efa1aea4c823c
Nate Catelli <ncatelli@packetfire.org>
2020-06-17 20:46:13 -0500

    Validate arity error.

--- src/interpreter/tests/expression/call.rs
+++ src/interpreter/tests/expression/call.rs
@@ -2,10 +2,10 @@ use crate::ast::expression::Expr;
 use crate::ast::statement::Stmt;
 use crate::functions;
 use crate::interpreter::Interpreter;
-use crate::interpreter::StatefulInterpreter;
+use crate::interpreter::{ExprInterpreterErr, StatefulInterpreter, StmtInterpreterErr};
 
 #[test]
-fn declaration_statement_should_set_persistent_global_symbol() {
+fn should_return_ok_on_success() {
     let f = functions::Function::new(
         vec![],
         Stmt::Block(vec![Stmt::Expression(Expr::Primary(obj_bool!(true)))]),
@@ -21,6 +21,29 @@ fn declaration_statement_should_set_persistent_global_symbol() {
         interpreter.interpret(vec![Stmt::Expression(Expr::Call(
             Box::new(Expr::Variable(tok_identifier!("a"))),
             vec![]
+        ))])
+    );
+}
+
+#[test]
+fn should_throw_error_on_arity_mismatch() {
+    let f = functions::Function::new(
+        vec![],
+        Stmt::Block(vec![Stmt::Expression(Expr::Primary(obj_bool!(true)))]),
+    );
+
+    let interpreter = StatefulInterpreter::new();
+    interpreter
+        .env
+        .define(&"a", obj_call!(Box::new(functions::Callable::Func(f))));
+
+    assert_eq!(
+        Err(StmtInterpreterErr::Expression(ExprInterpreterErr::CallErr(
+            "Arity".to_string()
+        ))),
+        interpreter.interpret(vec![Stmt::Expression(Expr::Call(
+            Box::new(Expr::Variable(tok_identifier!("a"))),
+            vec![Expr::Primary(obj_number!(5.0))]
         ))])
     );
 }
