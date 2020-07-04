99f70943d490c4d1a75d3a7213d7c8e5565a94e5
Nate Catelli <ncatelli@packetfire.org>
2020-07-04 14:15:39 -0500

    add analyze block.

--- src/analyzer/scope/mod.rs
+++ src/analyzer/scope/mod.rs
@@ -45,8 +45,17 @@ impl ScopeAnalyzer {
 impl From<Rc<Node>> for ScopeAnalyzer {
     fn from(scopes: Rc<Node>) -> ScopeAnalyzer {
         let mut si = ScopeAnalyzer::new();
-        si.scopes = scopes;
+        si.scopes = scopes.clone();
         si
+    }
+}
+
+/// SemanticAnalyzer<&Box<Expr>, usize> unpacks the boxed expr.
+impl SemanticAnalyzer<&Box<Expr>, usize> for ScopeAnalyzer {
+    type Error = ScopeAnalyzerErr;
+
+    fn analyze(&self, expr: &Box<Expr>) -> Result<usize, Self::Error> {
+        self.analyze(&(**expr))
     }
 }
 
@@ -57,16 +66,31 @@ impl SemanticAnalyzer<&Expr, usize> for ScopeAnalyzer {
 
     fn analyze(&self, expr: &Expr) -> Result<usize, Self::Error> {
         match expr {
-            Expr::Variable(id) => self.interpret_variable(id.clone()),
+            Expr::Variable(id) => self.analyze_variable(id.clone()),
+            Expr::Assignment(id, expr) => self.analyze_assignment(id.clone(), expr),
             _ => todo!(),
         }
     }
 }
 
 impl ScopeAnalyzer {
-    fn interpret_variable(&self, identifier: token::Token) -> Result<usize, ScopeAnalyzerErr> {
+    fn analyze_variable(&self, identifier: token::Token) -> Result<usize, ScopeAnalyzerErr> {
         let var = identifier.lexeme.unwrap();
 
+        match self.scopes.resolve_local(&var) {
+            Some(v) => Ok(v),
+            None => Err(ScopeAnalyzerErr::Undefined(var.to_string())),
+        }
+    }
+
+    fn analyze_assignment(
+        &self,
+        identifier: token::Token,
+        expr: &Expr,
+    ) -> Result<usize, ScopeAnalyzerErr> {
+        let var = identifier.lexeme.unwrap();
+
+        self.analyze(expr)?;
         match self.scopes.resolve_local(&var) {
             Some(v) => Ok(v),
             None => Err(ScopeAnalyzerErr::Undefined(var.to_string())),
@@ -95,9 +119,15 @@ impl SemanticAnalyzer<&Stmt, ()> for ScopeAnalyzer {
 
     fn analyze(&self, input: &Stmt) -> Result<(), Self::Error> {
         match input {
+            Stmt::Block(stmts) => self.analyze_block(stmts),
             _ => todo!(),
         }
     }
 }
 
-impl ScopeAnalyzer {}
+impl ScopeAnalyzer {
+    fn analyze_block(&self, stmts: &Vec<Stmt>) -> Result<(), ScopeAnalyzerErr> {
+        let block_analyzer = ScopeAnalyzer::from(Node::from(&self.scopes));
+        block_analyzer.analyze(stmts)
+    }
+}
