3d8e6ec96c284efac7058d132243a8e198609d0a
Nate Catelli <ncatelli@packetfire.org>
2020-07-07 21:21:35 -0500

    Implement a "nonce" analyser (#102)
    * Stub out an empty analyzer repo.
    
    * Strip extra env clones
    
    * Add tests to analyzer.

---
+++ src/analyzer/scope/tests/mod.rs
@@ -0,0 +1,82 @@
+use crate::analyzer::scope::ScopeAnalyzer;
+use crate::analyzer::SemanticAnalyzer;
+use crate::ast::expression::Expr;
+use crate::ast::statement::Stmt;
+
+#[test]
+fn expression_stmt_should_return_ok() {
+    let stmts = vec![Stmt::Expression(Expr::Primary(obj_bool!(true)))];
+
+    assert_eq!(Ok(stmts.clone()), ScopeAnalyzer::new().analyze(stmts));
+}
+
+#[test]
+fn print_stmt_should_return_self() {
+    let stmts = vec![Stmt::Print(Expr::Primary(obj_bool!(true)))];
+
+    assert_eq!(Ok(stmts.clone()), ScopeAnalyzer::new().analyze(stmts));
+}
+
+#[test]
+fn declaration_statement_should_return_self() {
+    let stmts = vec![Stmt::Declaration(
+        "test".to_string(),
+        Expr::Primary(obj_bool!(true)),
+    )];
+
+    assert_eq!(Ok(stmts.clone()), ScopeAnalyzer::new().analyze(stmts));
+}
+
+#[test]
+fn return_statement_should_return_self() {
+    let stmts = vec![Stmt::Return(Expr::Primary(obj_bool!(true)))];
+
+    assert_eq!(Ok(stmts.clone()), ScopeAnalyzer::new().analyze(stmts));
+}
+
+#[test]
+fn block_statement_should_return_self() {
+    let stmts = vec![Stmt::Block(vec![Stmt::Expression(Expr::Primary(
+        obj_bool!(true),
+    ))])];
+
+    assert_eq!(Ok(stmts.clone()), ScopeAnalyzer::new().analyze(stmts));
+}
+
+#[test]
+fn function_declaration_statement_should_return_self() {
+    let block = Stmt::Block(vec![Stmt::Expression(Expr::Primary(obj_bool!(true)))]);
+    let stmts = vec![Stmt::Function("test".to_string(), vec![], Box::new(block))];
+
+    assert_eq!(Ok(stmts.clone()), ScopeAnalyzer::new().analyze(stmts));
+}
+
+#[test]
+fn if_statement_should_return_self() {
+    let stmts = vec![Stmt::If(
+        Expr::Primary(obj_bool!(true)),
+        Box::new(Stmt::Declaration(
+            "test".to_string(),
+            Expr::Primary(obj_bool!(true)),
+        )),
+        Option::Some(Box::new(Stmt::Declaration(
+            "test".to_string(),
+            Expr::Primary(obj_bool!(false)),
+        ))),
+    )];
+
+    assert_eq!(Ok(stmts.clone()), ScopeAnalyzer::new().analyze(stmts));
+}
+
+#[test]
+fn while_statement_should_return_self() {
+    let stmts = vec![Stmt::While(
+        Expr::Primary(obj_bool!(false)),
+        Box::new(Stmt::Declaration(
+            "test".to_string(),
+            Expr::Primary(obj_bool!(true)),
+        )),
+    )];
+
+    assert_eq!(Ok(stmts.clone()), ScopeAnalyzer::new().analyze(stmts))
+}
--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -34,7 +34,7 @@ impl Environment {
     pub fn assign(&self, name: &str, value: object::Object) -> Option<object::Object> {
         let id = name.to_string();
         let has_key = self.symbols_table.borrow().contains_key(&id);
-        match (has_key, self.parent.clone()) {
+        match (has_key, self.parent.as_ref()) {
             (true, _) => self.define(&id, value),
             (false, Some(parent)) => parent.assign(&id, value),
             (false, None) => None,
@@ -51,7 +51,7 @@ impl Environment {
 
     pub fn get(&self, name: &str) -> Option<object::Object> {
         let val = self.symbols_table.borrow().get(name).cloned();
-        match (val, self.parent.clone()) {
+        match (val, self.parent.as_ref()) {
             (Some(v), _) => Some(v),
             (None, Some(parent)) => parent.get(&name),
             (None, None) => None,
