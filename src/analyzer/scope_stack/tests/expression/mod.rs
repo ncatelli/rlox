4e150729987969a23539b97e9e8ab7c595ed8b3b
Nate Catelli <ncatelli@packetfire.org>
2020-07-15 23:19:24 -0500

    Add lambda expression test.

--- src/analyzer/scope_stack/tests/expression/mod.rs
+++ src/analyzer/scope_stack/tests/expression/mod.rs
@@ -1,6 +1,7 @@
 use crate::analyzer::scope_stack::ScopeAnalyzer;
 use crate::analyzer::SemanticAnalyzerMut;
 use crate::ast::expression::Expr;
+use crate::ast::statement::Stmt;
 
 #[test]
 fn primary_expression_should_return_ok() {
@@ -75,6 +76,28 @@ fn call_analyze_should_resolve_identifiers_to_ids() {
         Box::new(Expr::Variable(identifier_id!(0))),
         vec![Expr::Variable(identifier_id!(1))],
     );
+
+    assert_eq!(Ok(output), sa.analyze(input));
+}
+
+#[test]
+fn lambda_expression_should_match_predefined_value() {
+    let mut sa = ScopeAnalyzer::new();
+    let input = Expr::Lambda(
+        vec![identifier_name!("test"), identifier_name!("test_again")],
+        Box::new(Stmt::Block(vec![Stmt::Print(Expr::Variable(
+            identifier_name!("test"),
+        ))])),
+    );
+    let output = Expr::Lambda(
+        vec![identifier_id!(1), identifier_id!(2)],
+        Box::new(Stmt::Block(vec![Stmt::Print(Expr::Variable(
+            identifier_id!(1),
+        ))])),
+    );
+
+    // Pre-declare a test variable for the above assignment to assign to
+    sa.declare_or_assign(identifier_name!("hello"));
 
     assert_eq!(Ok(output), sa.analyze(input));
 }
