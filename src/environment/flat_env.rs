e939e8c348d06f41bc29d50f8a2d7ed94396d0c0
Nate Catelli <ncatelli@packetfire.org>
2021-03-05 04:03:01 +0000

    Implement GC

--- src/environment/flat_env.rs
+++ src/environment/flat_env.rs
@@ -1,4 +1,81 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, usize};
+
+#[derive(Default, Clone)]
+/// SortedSet maintains a sorted list of input values.
+struct SortedSet<T>
+where
+    T: Ord,
+{
+    v: Vec<T>,
+    s: std::collections::HashSet<T>,
+}
+
+impl<T> SortedSet<T>
+where
+    T: Eq + std::hash::Hash + Clone + Copy + Ord,
+{
+    fn len(&self) -> usize {
+        self.v.len()
+    }
+
+    /// If an element doesn't currently exist in a set, it is inserted into
+    /// the set in order.
+    fn insert(&mut self, value: T) -> bool {
+        if self.s.insert(value.clone()) {
+            self.v.push(value);
+            self.v.sort_unstable();
+            true
+        } else {
+            false
+        }
+    }
+
+    /// If an element exists in a set, it is removed, returning true.
+    fn remove(&mut self, value: T) -> bool {
+        if self.s.remove(&value) {
+            self.v = self.v.iter().copied().filter(|&x| x != value).collect();
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Returns a reference to the first element of the set if it exists.
+    fn first(&self) -> Option<&T> {
+        self.v.first()
+    }
+
+    /// Returns a reference to the last element of the set if it exists.
+    fn last(&self) -> Option<&T> {
+        self.v.last()
+    }
+}
+
+impl<T> SortedSet<T>
+where
+    T: Clone + Copy + Ord,
+{
+    fn iter(&self) -> std::slice::Iter<T> {
+        self.v.iter()
+    }
+}
+
+impl<T> std::fmt::Debug for SortedSet<T>
+where
+    T: std::fmt::Debug + Clone + Copy + Ord,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "OrderedSet{{{}}}",
+            self.clone()
+                .iter()
+                .map(|t| format!("{:?}", t))
+                .collect::<Vec<String>>()
+                .join(", ")
+        )
+    }
+}
 
 #[derive(Default, Clone)]
 /// OrderedSet maintains a consistent order of items determined by the sequence
@@ -17,6 +94,16 @@ where
     fn insert(&mut self, value: T) -> bool {
         if self.s.insert(value.clone()) {
             self.v.push(value);
+            true
+        } else {
+            false
+        }
+    }
+
+    /// If an element exists in a set, it is removed, returning true.
+    fn remove(&mut self, value: T) -> bool {
+        if self.s.remove(&value) {
+            self.v = self.v.iter().copied().filter(|&x| x != value).collect();
             true
         } else {
             false
@@ -57,6 +144,8 @@ type ScopeId = usize;
 pub struct Environment<V> {
     /// Stores scope indexed variable maps.
     data: Vec<HashMap<String, V>>,
+    /// Tracks active scopes for use in GC.
+    active: SortedSet<ScopeId>,
     /// a parent of a scope indexed on the array id.
     parents: Vec<Option<ScopeId>>,
     /// stores a hashset of all child nodes of a scope aligned on the scope id.
@@ -72,9 +161,18 @@ where
     pub fn new() -> Self {
         Self {
             data: vec![HashMap::new()],
+            active: {
+                let mut set = SortedSet::default();
+                set.insert(0);
+                set
+            },
             children: vec![OrderedSet::default()],
             parents: vec![None],
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.active.len()
     }
 
     /// Generates a new scope in place, linking the new scope to it's parent.
@@ -83,6 +181,7 @@ where
         let scope_id = self.data.len();
         // instantiate scope
         self.data.push(HashMap::new());
+        self.active.insert(scope_id);
         self.children.push(OrderedSet::default());
         self.parents.push(Some(parent));
 
@@ -97,6 +196,47 @@ where
     pub fn add_scope(mut self, parent: ScopeId) -> (Self, ScopeId) {
         let scope_id = self.add_scope_mut(parent);
         (self, scope_id)
+    }
+
+    pub fn drop_scope_mut(&mut self, scope_id: ScopeId) -> ScopeId {
+        // instantiate scope
+        self.data[scope_id] = HashMap::new();
+        self.active.remove(scope_id);
+        self.children[scope_id] = OrderedSet::default();
+        self.parents[scope_id].map(|parent_scope| self.children[parent_scope].remove(scope_id));
+
+        // return the index into the data vector representing the new scope.
+        scope_id
+    }
+
+    /// Wraps drop_scope_mut, returning both the environment and the removed scope_id.
+    pub fn drop_scope(mut self, scope_id: ScopeId) -> (Self, ScopeId) {
+        let scope_id = self.drop_scope_mut(scope_id);
+        (self, scope_id)
+    }
+
+    /// Resizes the environment based on active scopes. If a resize occurs,
+    /// the new size is returned, otherwise None is returned for no action.
+    fn garbage_collect_mut(&mut self) -> Option<usize> {
+        let minimum_scope_len = self.active.last().unwrap_or(&0) + 1;
+        let desired_capacity = minimum_scope_len * 2;
+        let current_length = self.data.len();
+
+        if current_length > desired_capacity {
+            self.data.resize(desired_capacity, HashMap::new());
+            self.parents.resize(desired_capacity, None);
+            self.children
+                .resize(desired_capacity, OrderedSet::default());
+            Some(desired_capacity)
+        } else {
+            None
+        }
+    }
+
+    /// Wraps garbage_collect_mut, returning both the resized environment and new_desired_size.
+    pub fn garbage_collect(mut self) -> (Self, Option<usize>) {
+        let new_size = self.garbage_collect_mut();
+        (self, new_size)
     }
 
     /// Returns the parent scope id for a given scope, or None if the scope is
@@ -256,5 +396,32 @@ mod tests {
 
         assert_eq!(Some(5), previous_value);
         assert_eq!(Some(&10), env.data[0].get("test"));
+    }
+
+    #[test]
+    fn should_resize_down_when_active_scopes_shrinks_below_half_capacity() {
+        // generate 50 children off root.
+        let e = (1..50)
+            .into_iter()
+            .fold(Environment::<usize>::new(), |e, _| e.add_scope(0).0);
+
+        assert_eq!(50, e.data.len());
+
+        // mark all but the first 10 scopes inactive
+        let oversized_env = (10..50)
+            .rev()
+            .into_iter()
+            .fold(e, |e, scope_id| e.drop_scope(scope_id).0);
+
+        assert_eq!(10, oversized_env.len());
+        assert_eq!(50, oversized_env.data.len());
+
+        // run a gc
+        let (gc_env, target_size) = oversized_env.garbage_collect();
+        assert_eq!(Some(20), target_size);
+        assert_eq!(20, gc_env.data.len());
+
+        // subsequent runs should return none for targetsize
+        assert_eq!(None, gc_env.garbage_collect().1);
     }
 }
